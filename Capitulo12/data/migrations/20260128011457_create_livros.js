462ea4067633dfab8006dbd5ba22df358b039f56
Lucas Henrique <lucasvieiragamer768@gmail.com>
2026-01-27 22:45:03 -0300

    ADD database

--- Capitulo12/data/migrations/20260128011457_create_livros.js
+++ Capitulo12/data/migrations/20260128011457_create_livros.js
@@ -3,7 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("livros", (table) => {
+    table.increments()
+    table.string("titulo", 80).notNullable()
+    table.string("autor", 60).notNullable()
+    table.integer("ano", 4).notNullable()
+    table.decimal("preco", 9.2).notNullable()
+    table.string("foto", 100).notNullable()
+  })
 };
 
 /**
@@ -11,5 +18,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("livros")
 };
